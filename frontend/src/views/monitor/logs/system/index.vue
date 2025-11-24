3153243b31df42c967c07e652ff9e7b9f032a62e
pylover <1372755472@qq.com>
2025-11-24 14:32:54 +0800

    🎈 perf: 优化系统日志查询表单，统一操作时间参数命名并简化表格标题

--- frontend/src/views/monitor/logs/system/index.vue
+++ frontend/src/views/monitor/logs/system/index.vue
@@ -52,9 +52,9 @@ const {
           class="w-[170px]!"
         />
       </el-form-item>
-      <el-form-item label="请求时间" prop="requestTime">
+      <el-form-item label="请求时间" prop="oprationTime">
         <el-date-picker
-          v-model="form.requestTime"
+          v-model="form.oprationTime"
           :shortcuts="getPickerShortcuts()"
           type="datetimerange"
           range-separator="至"
@@ -77,11 +77,7 @@ const {
       </el-form-item>
     </el-form>
 
-    <PureTableBar
-      title="系统日志（仅演示，操作后不生效）"
-      :columns="columns"
-      @refresh="onSearch"
-    >
+    <PureTableBar title="系统日志" :columns="columns" @refresh="onSearch">
       <template #buttons>
         <el-popconfirm title="确定要删除所有日志数据吗？" @confirm="clearAll">
           <template #reference>
