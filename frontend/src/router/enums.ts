b668f032f8d43c8914c231bb52ef04957ad223ec
pylover <1372755472@qq.com>
2025-12-04 21:51:51 +0800

    ♻️ refactor: 移除前端路由枚举中未使用的 system 和 monitor 常量

--- frontend/src/router/enums.ts
+++ frontend/src/router/enums.ts
@@ -2,8 +2,6 @@
 
 const home = 0, // 平台规定只有 home 路由的 rank 才能为 0 ，所以后端在返回 rank 的时候需要从非 0 开始
   error = 110,
-  system = 114,
-  monitor = 115,
   about = 999;
 
-export { home, error, system, monitor, about };
+export { home, error, about };
