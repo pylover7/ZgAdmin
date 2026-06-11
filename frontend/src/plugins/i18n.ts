f8408e9c153e35e18af8c75945e38889c6026421
pylover <shiye@pylover.net>
2026-06-12 00:42:00 +0800

    refactor(i18n): 简化国际化转换逻辑

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -2,7 +2,7 @@
 import { type I18n, createI18n } from "vue-i18n";
 import type { App, WritableComputedRef } from "vue";
 import { responsiveStorageNameSpace } from "@/config";
-import { storageLocal, isObject } from "@pureadmin/utils";
+import { storageLocal } from "@pureadmin/utils";
 
 // element-plus国际化
 import enLocale from "element-plus/es/locale/lang/en";
@@ -36,40 +36,15 @@ export const localesConfigs = {
   }
 };
 
-/** 获取对象中所有嵌套对象的key键，并将它们用点号分割组成字符串 */
-function getObjectKeys(obj) {
-  const stack = [];
-  const keys: Set<string> = new Set();
-
-  stack.push({ obj, key: "" });
-
-  while (stack.length > 0) {
-    const { obj, key } = stack.pop();
-
-    for (const k in obj) {
-      const newKey = key ? `${key}.${k}` : k;
-
-      if (obj[k] && isObject(obj[k])) {
-        stack.push({ obj: obj[k], key: newKey });
-      } else {
-        keys.add(newKey);
-      }
-    }
-  }
-
-  return keys;
-}
-
-/** 将展开的key缓存 */
-const keysCache: Map<string, Set<string>> = new Map();
-const flatI18n = (prefix = "zh-CN") => {
-  let cache = keysCache.get(prefix);
-  if (!cache) {
-    cache = getObjectKeys(siphonI18n(prefix));
-    keysCache.set(prefix, cache);
-  }
-  return cache;
-};
+export const i18n: I18n = createI18n({
+  legacy: false,
+  locale:
+    storageLocal().getItem<StorageConfigs>(
+      `${responsiveStorageNameSpace()}locale`
+    )?.locale ?? "zh",
+  fallbackLocale: "en",
+  messages: localesConfigs
+});
 
 /**
  * 国际化转换工具函数（自动读取根目录locales文件夹下文件进行国际化匹配）
@@ -88,30 +63,11 @@ export function transformI18n(message: any = "") {
     return message[locale?.value];
   }
 
-  const key = message.match(/(\S*)\./)?.input;
-
-  if (key && flatI18n("zh-CN").has(key)) {
-    return i18n.global.t.call(i18n.global.locale, message);
-  } else if (!key && Object.hasOwn(siphonI18n("zh-CN"), message)) {
-    // 兼容非嵌套形式的国际化写法
-    return i18n.global.t.call(i18n.global.locale, message);
-  } else {
-    return message;
-  }
+  return (i18n.global.t as any)(message);
 }
 
 /** 此函数只是配合i18n Ally插件来进行国际化智能提示，并无实际意义（只对提示起作用），如果不需要国际化可删除 */
 export const $t = (key: string) => key;
-
-export const i18n: I18n = createI18n({
-  legacy: false,
-  locale:
-    storageLocal().getItem<StorageConfigs>(
-      `${responsiveStorageNameSpace()}locale`
-    )?.locale ?? "zh",
-  fallbackLocale: "en",
-  messages: localesConfigs
-});
 
 export function useI18n(app: App) {
   app.use(i18n);
